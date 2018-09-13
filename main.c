4635aee31026efc56c6a5279ec83dd8d7e92509b
Davis <td916215@ohio.edu>
2018-09-13 18:29:43 -0400

    first commit

---
+++ main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    // Variable declaration and assignment.
+    int a;
+    a = 1979;
+
+    // Variable declaration and initialization.
+    int b = 1986;
+    int c = a;
+
+
+    printf("Value of a = %d\nValue of b = %d\nValue of c = %d\n",a,b,c);
+
+    // Alternate way of writing the print function above.
+    //printf("Value of a = %d\n",a);
+    //printf("Value of b = %d\n",b);
+    //printf("Value of c = %d \n",c);
+
+    /* This is another way to disable code.
+    So is this */
+
+
+    //printf("Hello world!\n");
+
+    return 0;
+}
